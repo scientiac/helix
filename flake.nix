26af2f89c71898031a8f877536e62f2af735f4f4
scientiac <iacsci@proton.me>
2025-03-11 14:14:09 +0545

    remove: cacheix to compile own?

--- flake.nix
+++ flake.nix
@@ -83,9 +83,4 @@
         helix = final.callPackage ./default.nix {inherit gitRev;};
       };
     };
-
-  nixConfig = {
-    extra-substituters = ["https://helix.cachix.org"];
-    extra-trusted-public-keys = ["helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="];
-  };
 }
